7dab1bfaa1baff7f7eedc0bf93cb09aadd077872
mike <adegboyegamichael291@gmail.com>
2026-06-18 05:37:01 +0100

    Force IPv4 DNS globally to fix SMTP ENETUNREACH on Render

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
